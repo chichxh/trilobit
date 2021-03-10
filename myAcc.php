7e4eb74ec1af583c0e5b540d7d1cd8ddf60ef4b2
{доена} <egorodand@gmail.com>
2021-03-11 03:49:35 +0900

    fix myAcc.php

--- myAcc.php
+++ myAcc.php
@@ -21,14 +21,25 @@ if ($link == false){
 <head>
 	<title>Трилобит - Городская среда</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-	<style type="text/css"></style>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Montserrat:wght@300&display=swap" rel="stylesheet">
+	<style type="text/css">
+		body {
+			font-family: 'Montserrat', Arial;
+			color: #fff;
+			background-color: #2A363B;
+		}
+	</style>
 </head>
 <body>
 	<?php include('header.php')?>
 	<div class="container">
-		<h1>Если вы хотите посчитать ваши расходы сразу используйте это</h1>
+		<h1>Если вы хотите посчитать ваши расходы автоматически, используйте это</h1>
 	</div>
 	<div class="container mt-5">
+		<div class="col-6 mb-5">
+			<button id="generateQRCode" class="btn btn-light">Сгенерировать код</button>
+		</div>
 		<form action="myAcc.php" method="POST">
 			<div class="mb-3">
 				<label for="exampleFormControlTextarea1" class="form-label"></label>
@@ -40,14 +51,11 @@ if ($link == false){
 			</div>
 			<button name="send" class="btn btn-light">Начать отсчет</button>
 		</form>
-		<div class="col-6 mt-4">
-			<button id="generateQRCode" class="btn btn-light">Сгенерировать код</button>
-		</div>
 	</div>
     <div class="container">
     	<div class="row d-flex justify-content-center">
     		<div id="qrCodeOutput" class="mt-5">
-		        <p>Нажми на кнопку, чтобы увидеть свой персональный код, можете сделать скриншот</p>
+		        <p>Нажми на кнопку, чтобы увидеть свой персональный код, можете сделать скриншот, также лучше не обновляйте страницу</p>
 		    </div>
     	</div>
     </div>
