737c36ad7ba284d4c9b066fa71195f3066396131
{доена} <egorodand@gmail.com>
2021-03-11 16:43:20 +0900

    add bottle.php merch.php

---
+++ bottle.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Трилобит - Городская среда</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Montserrat:wght@300&display=swap" rel="stylesheet">
+	<style type="text/css">
+		body {
+			font-family: 'Montserrat', Arial;
+			color: #fff;
+			background-color: #2A363B;
+		}
+		.round {
+			background-color: #1A2326;
+			border-radius: 30px;
+			padding: 20px 0;
+			height: 480px;
+		}
+	</style>
+</head>
+<body>
+	<?php include('header.php')?>
+
+	<div class="container-fluid mt-5">
+		<div class="row">
+			<div class="col-4">
+				<h1 class="text-center">Бутылка</h1>
+			</div>
+			<div class="col-4 text-center">
+			    <img src="img/bottle.png" width="100">
+			</div>
+			<div class="col-4">
+				<h1 class="text-center">Коламбии</h1>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-4">
+				<h6 class="text-center mt-5">Васильева Анна <br></h6>
+				<h6 class="text-center mt-3">Менеджер</h6>
+				<h6 class="text-center mt-3"><a href="tel:89142234474">89142234474</a></h6>
+			</div>
+			<div class="col-4">
+				<h6 class="text-center mt-5">Лыткин Владимир<br></h6>
+				<h6 class="text-center mt-3">Дизайнер</h6>
+				<h6 class="text-center mt-3"><a href="tel:+79841025713">+79841025713</a></h6>
+			</div>
+			<div class="col-4">
+				<h6 class="text-center mt-5">Егорова Дайаана<br></h6>
+				<h6 class="text-center mt-3">Разработчик</h6>
+				<h6 class="text-center mt-3"><a href="tel:89142869159">89142869159</a></h6>
+			</div>
+		</div>
+	</div>	
+</body>
+</html>
---
+++ merch.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Трилобит - Городская среда</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Montserrat:wght@300&display=swap" rel="stylesheet">
+	<style type="text/css">
+		body {
+			font-family: 'Montserrat', Arial;
+			color: #fff;
+			background-color: #2A363B;
+		}
+		.round {
+			background-color: #1A2326;
+			border-radius: 30px;
+			padding: 20px 0;
+			height: 480px;
+	</style>
+</head>
+<body>
+	<?php include('header.php')?>
+
+	<h1 class="text-center fw-bold mt-5">Мерч</h1>
+	<div class="container">
+		<div class="row">
+			<div class="col-5 round mx-auto">
+				<h1 class="text-center">Футболка</h1>
+			</div>
+			<div class="col-5 round mx-auto">
+				<h1 class="text-center">Кепка</h1>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
